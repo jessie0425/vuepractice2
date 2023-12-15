b55ac040cfe7e2b77a56d044440d873f8862c2d7
石喬昕 <jessieshi0425@gmail.com>
2023-12-15 14:50:40 +0800

    載入vue axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios';
 
+// 將 axios 掛載到 Vue 實例原型上
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   router,
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
-    <h1>This is an about page</h1>
+    <h1>登入</h1>
+    <el-row :gutter="20">
+      <el-col :span="12" :offset="6">
+        <el-form :model="dynamicValidateForm" ref="dynamicValidateForm" label-width="100px" class="demo-dynamic">
+          <el-form-item
+              prop="email"
+              label="信箱"
+              :rules="[
+      { required: true, message: '請輸入信箱', trigger: 'blur' },
+      { type: 'email', message: '請輸入正確的信箱格式', trigger: ['blur', 'change'] }
+    ]"
+          >
+            <el-input v-model="dynamicValidateForm.email"></el-input>
+          </el-form-item>
+          <el-form-item
+              label="密碼"
+              prop="password"
+              :rules="[
+      { required: true, message: '請輸入密碼', validator: validatePass, trigger: 'blur' },
+    ]">
+            <el-input type="password" v-model="dynamicValidateForm.password" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="login('dynamicValidateForm')">提交</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -8,14 +35,31 @@
     name: 'LoginView',
     data(){
       return{
-        user:{
-          email:'',
-          password:''
-        }
+        dynamicValidateForm: {
+          email: '',
+          password: ''
+        },
       }
     },
     methods:{
-
+      validatePass(rule, value, callback){
+        if (value === '') {
+          callback(new Error('請輸入密碼'));
+        } else if(value.length <= 5){
+          callback(new Error('密碼要大於6個字元'));
+        } else {
+          callback();
+        }
+      },
+      login(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('提交');
+          } else {
+            return false;
+          }
+        });
+      }
     }
   }
 </script>
